3dd3a4a175ceff95e65ec25b3fbea55b6f88ca2b
MihaelHoli <mholi@student.unipu.hr>
2025-06-06 15:33:31 +0200

    japan

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'physarum-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './index.js',
+  './index.pck',
+  './index.wasm',
+  './index.icon.png',
+  './index.apple-touch-icon.png',
+  './index.png',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
